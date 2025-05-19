60eb8fc0e0acb893921ce1a9d5fc7544876f88f1
zeinab <zeinabhteit1@gmail.com>
2025-05-19 12:45:37 +0300

    updated coes

--- eco_front/vite.config.js
+++ eco_front/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-})
+  // Add this base configuration
+  base: "/",
+  // Optional: Configure the build output
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+});
